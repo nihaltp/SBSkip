1f76e3501fb06f9db150efce80deff2054c5d412
NIHAL  T P <nihaltpnki@gmail.com>
2026-05-30 00:09:17 +0530

    chore: bump version to 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
         applicationId = "com.nihaltp.sbskip"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
+        versionCode = 1
         versionName = "1.0.0"
 
         ndk {
