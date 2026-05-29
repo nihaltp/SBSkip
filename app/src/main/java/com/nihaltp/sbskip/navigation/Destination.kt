f686b0058a6ecb27d6b36731b25b443b1b67ea23
NIHAL  T P <nihaltpnki@gmail.com>
2026-05-30 02:25:59 +0530

    refactor: change Destination class from sealed to abstract and update Android Gradle plugin version

--- app/src/main/java/com/nihaltp/sbskip/navigation/Destination.kt
+++ app/src/main/java/com/nihaltp/sbskip/navigation/Destination.kt
@@ -1,6 +1,6 @@
 package com.nihaltp.sbskip.navigation
 
-sealed class Destination(val route: String) {
+abstract class Destination(val route: String) {
     data object Home : Destination("home")
     data object DownloadConfig : Destination("download_config")
     data object Settings : Destination("settings")
