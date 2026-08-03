19d74e338c7b6d87a8f578310fce53ad7335073e
NIHAL  T P <nihaltpnki@gmail.com>
2026-08-03 15:05:26 +0530

    chore: bump version to v1.8.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "com.nihaltp.sbskip"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.8.0"
+        versionCode = 14
+        versionName = "1.8.1"
 
         val changelogsDir = rootProject.file("fastlane/metadata/android/en-US/changelogs")
         var latestChangelog = ""
