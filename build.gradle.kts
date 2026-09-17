8c20074da20aceb1ef2aa890d182ec2ff0664cd0
NIHAL  T P <nihaltpnki@gmail.com>
2026-09-18 01:36:45 +0530

    chore: update Android Gradle plugin version to 8.10.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "8.8.2" apply false
+    id("com.android.application") version "8.10.1" apply false
     id("org.jetbrains.kotlin.android") version "2.2.0" apply false
     id("org.jetbrains.kotlin.kapt") version "2.2.0" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.2.0" apply false
